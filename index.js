595ef563090bfdc63d13dddcca9ae7da312044d7
Victor Ferraz <ferrazvictor10@gmail.com>
2023-03-06 22:09:07 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const { request, response } = require('express');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/',(request, response) =>{
+    return response.json({message: 'Server is up'});
+})
+
+app.post('/teste', (request, response) => {
+    const {name, date} = request.body;
+    return response.json({name, date});
+})
+
+app.listen(3333)
